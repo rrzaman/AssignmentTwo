bd3baf521be27ca3bafdfb0fad951ade315a9611
rrzaman <rayyan.rz5@gmail.com>
2022-07-16 11:52:31 -0700

    Third commit; added public access modifier to countEvenNum method. Has no effect here but just for good practice.

--- src/program/main.java
+++ src/program/main.java
@@ -16,7 +16,7 @@ public class main {
 
     }
 
-    static int countEvenNum(int[] arr, int size) {
+    public static int countEvenNum(int[] arr, int size) {
         int count = 0;
 
         for (int i = 0; i < arr.length; i++) {
